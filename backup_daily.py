5303703ba31b08ea4cb33898e6ee340dc65cafce
Sherabi <nightfader@gmail.com>
2013-05-03 14:13:48 -0400

    Got rid of globals, made almost all variables local and removed CAPITALIZATION. Major update

--- backup_daily.py
+++ backup_daily.py
@@ -33,158 +33,148 @@ parser = SafeConfigParser()
 parser.read(config)
 
 RSYNC='rsync -avR --delete --stats'
-BACKUP_LOG_DAILY = "junk"
-BACKUP_MAIL = "junk_mail"
-DESTINATION = "foo"
-SERVER = "whatever"
 
 def parse_config():
-	global BACKUP_LOG_DAILY
-	global BACKUP_MAIL
-	global DESTINATION
-	global SERVER
 	for section_name in parser.sections():
-
-		SERVER = section_name
-
+		server = section_name
 		if parser.has_option(section_name, 'log_dir'):
 			# Create the log directory.
-			LOG_DIR = parser.get(section_name, 'log_dir')
-			if not os.path.exists(LOG_DIR):
-				os.makedirs(LOG_DIR, 0755)
+			log_dir = parser.get(section_name, 'log_dir')
+			if not os.path.exists(log_dir):
+				os.makedirs(log_dir, 0755)
 			# Initialize backup mail file
-			BACKUP_MAIL = LOG_DIR+"/backup_mail_daily"
-			BACKUP_LOG_DAILY = LOG_DIR + "/backup_log_daily_" + SERVER
-			INCLUDE_FILENAME =  LOG_DIR + "/backup_includes_daily_" + SERVER
-			EXCLUDE_FILENAME =  LOG_DIR + "/backup_excludes_daily_" + SERVER
-			bkout = open(BACKUP_LOG_DAILY, 'w+', 0)
+			daily_backup_mail = log_dir+"/backup_mail_daily"
+			daily_log = log_dir + "/backup_log_daily_" + server
+			include_filename =  log_dir + "/backup_includes_daily_" + server
+			exclude_filename =  log_dir + "/backup_excludes_daily_" + server
+			bkout = open(daily_log, 'w+', 0)
 		else:
 			print 'log_dir not set for section %s' % (section_name)
 			continue
 		
 		if parser.has_option(section_name, 'backup_type'):
-			LOCATION = parser.get(section_name, 'backup_type')
+			location = parser.get(section_name, 'backup_type')
 		else:
 			print 'backup_type not set for section %s' % (section_name)
 			bkout.write('backup_type not available for section %s' % (section_name))
 			continue
 
 		if parser.has_option(section_name, 'include'):
-			INCLUDES = parser.get(section_name, 'include')
+			includes = parser.get(section_name, 'include')
 		else:
 			print 'include not set for section %s' % (section_name)
 			bkout.write('include not set for section %s' % (section_name))
 			continue
 
 		if parser.has_option(section_name, 'exclude'):
-			EXCLUDES = parser.get(section_name, 'exclude')
+			excludes = parser.get(section_name, 'exclude')
 		else:
 			print 'exclude not available for section %s' % (section_name)
 			bkout.write('exclude not set for section %s' % (section_name))
 			continue
 
 		if parser.has_option(section_name, 'retention'):
-			RETENTION = parser.get(section_name, 'retention')
+			retention = parser.get(section_name, 'retention')
 		else:
 			print 'retention not set for section %s' % (section_name)
 			bkout.write('retention not set for section %s' % (section_name))
 			continue
 
 		if parser.has_option(section_name, 'backup_location'):
-			DESTINATION = parser.get(section_name, 'backup_location')
+			destination = parser.get(section_name, 'backup_location')
 			# Create Target directory.
-			if not os.path.exists(DESTINATION):
-				os.makedirs(DESTINATION)
+			if not os.path.exists(destination):
+				os.makedirs(destination)
 		else:
 			print 'backup_location not set for section %s' % (section_name)
 			bkout.write('backup_location not set for section %s' % (section_name))
 			continue
 
 		if parser.has_option(section_name, 'interval'):
-			INTERVAL = parser.get(section_name, 'interval')
+			interval = parser.get(section_name, 'interval')
 		else:
 			print 'interval not set for section %s' % (section_name)
 			bkout.write('interval not set for section %s' % (section_name))
 			continue
 
 		if parser.has_option(section_name, 'username'):
-			USER = parser.get(section_name, 'username')
+			user = parser.get(section_name, 'username')
 		else:
 			print 'username not available for section %s' % (section_name)
 			bkout.write('username not available for section %s' % (section_name))
 			continue
 
 		if parser.has_option(section_name, 'non_admin_emails'):
-			MAIL_LIST = parser.get(section_name, 'non_admin_emails')
+			mail_list = parser.get(section_name, 'non_admin_emails')
 		else:
 			print 'non_admin_emails not available for section %s' % (section_name)
 			bkout.write('non_admin_emails not set for section %s' % (section_name))
 			continue
 
 		if parser.has_option(section_name, 'admin_emails'):
-			ADMIN_EMAILS = parser.get(section_name, 'admin_emails')
+			admin_emails = parser.get(section_name, 'admin_emails')
 		else:
 			print 'admin_emails not available for section %s' % (section_name)
 			bkout.write('admin_emails not set for section %s' % (section_name))
 			continue
 
 		if parser.has_option(section_name, 'disk_alert'):
-			DISK_ALERT = parser.get(section_name, 'disk_alert')
+			disk_alert = parser.get(section_name, 'disk_alert')
 		else:
 			print 'disk_alert not available for section %s' % (section_name)
 			bkout.write('disk_alert not set for section %s' % (section_name))
 			continue
 
 		if parser.has_option(section_name, 'disk_report'):
-			DISK_REPORT = parser.get(section_name, 'disk_report')
+			disk_report = parser.get(section_name, 'disk_report')
 		else:
 			print 'disk_report not available for section %s' % (section_name)
 			bkout.write('disk_report not set for section %s' % (section_name))
 			continue
 
-	 	INCLUDE_FILE = open(INCLUDE_FILENAME, 'w+')
-		for inc in INCLUDES.split(','):
-			INCLUDE_FILE.write(inc.lstrip(' ')+"\n")
-		INCLUDE_FILE.close()
+	 	include_file = open(include_filename, 'w+')
+		for inc in includes.split(','):
+			include_file.write(inc.lstrip(' ')+"\n")
+		include_file.close()
 		
-		EXCLUDE_FILE = open(EXCLUDE_FILENAME, 'w+')
-		for exc in EXCLUDES.split(','):
-			EXCLUDE_FILE.write(exc.lstrip(' ')+"\n")
-		EXCLUDE_FILE.close()
+		exclude_file = open(exclude_filename, 'w+')
+		for exc in excludes.split(','):
+			exclude_file.write(exc.lstrip(' ')+"\n")
+		exclude_file.close()
 
-		if (len(USER) == 0):
+		if (len(user) == 0):
 			print "username field is empty"
 			bkout.write("username field is empty")
 			continue
 
-		if (len(DESTINATION) > 0):
+		if (len(destination) > 0):
 			now = datetime.datetime.now()
 			rightnow = now.strftime("%Y-%m-%d@%H:%M:%S")
-			print "=== START OF BACKUPS FOR %s ===\n" %(SERVER)
-			print "*** BACKUPS FOR %s STARTED %s ***" %(SERVER, rightnow)
-			print "Destination: %s" %(DESTINATION)
-			bkout.write("=== START OF BACKUPS FOR %s ===\n" %(SERVER))
-			bkout.write("*** BACKUPS FOR %s STARTED %s ***\n" %(SERVER, rightnow))
-			bkout.write("Destination: %s\n" %(DESTINATION))
+			print "=== START OF BACKUPS FOR %s ===\n" %(server)
+			print "*** BACKUPS FOR %s STARTED %s ***" %(server, rightnow)
+			print "Destination: %s" %(destination)
+			bkout.write("=== START OF BACKUPS FOR %s ===\n" %(server))
+			bkout.write("*** BACKUPS FOR %s STARTED %s ***\n" %(server, rightnow))
+			bkout.write("Destination: %s\n" %(destination))
 		else:
 			print "backup_location field is empty"
 			bkout.write("backup_location field is empty")
 			continue
 
 		bkout.flush()
-		disk_space_check(DESTINATION, DISK_ALERT, DISK_REPORT, ADMIN_EMAILS, SERVER)
+		disk_space_check(destination, disk_alert, disk_report, admin_emails, server)
 		try:
 			bkout.flush()
-			delete_old(RETENTION, SERVER, DESTINATION+"/"+SERVER+"/daily")
+			delete_old(retention, server, destination+"/"+server+"/daily", daily_log)
 		except:
 			print "Directory to delete does not exist"
 			bkout.write("Directory to delete does not exist\n")
 			pass
-		if LOCATION == "remote":
-			backup_remote(SERVER, DESTINATION, INCLUDE_FILENAME, EXCLUDE_FILENAME, USER)
+		if location == "remote":
+			backup_remote(server, destination, include_filename, exclude_filename, user, daily_log)
 		else:
-			backup_local(SERVER, DESTINATION, INCLUDE_FILENAME, EXCLUDE_FILENAME)
-		copy_to_today(SERVER, DESTINATION, EXCLUDE_FILENAME)
+			backup_local(server, destination, include_filename, exclude_filename, daily_log)
+		copy_to_today(server, destination, exclude_filename, daily_log)
 		bkout.close()
 
 def disk_space_check(directory, alert_threshold, disk_report, admin_emails, server):
@@ -204,11 +194,11 @@ def disk_space_check(directory, alert_threshold, disk_report, admin_emails, serv
 					used_percentage = int(use_perc.replace(char,''))
 					if (used_percentage > int(alert_threshold)):
 						now = datetime.datetime.now()
-						backup_mail = open(BACKUP_MAIL, 'a+')
+						backup_mail = open(daily_backup_mail, 'a+')
 						backup_mail.write('Running out of space on ' + hostname + ' for partition ' + partition + ' as of ' + now.strftime("%Y-%m-%d@%H:%M:%S") + '.\n')
 						backup_mail.write('Threshold for disk space was reached at ' + str(alert_threshold) + '% backup was aborted for ' + server)
 						backup_mail.close()
-						mail_str = 'mailx -s  \"Alert: Almost out of disk space, backups ABORTED - ' + str(used_percentage) + '%\" ' + admin_emails + " < " + BACKUP_MAIL
+						mail_str = 'mailx -s  \"Alert: Almost out of disk space, backups ABORTED - ' + str(used_percentage) + '%\" ' + admin_emails + " < " + daily_backup_mail
 						subprocess.Popen(mail_str, shell=True)
 						print 'Running out of space on ' + hostname + ' for partition ' + partition + ' as of ' + now.strftime("%Y-%m-%d@%H:%M:%S") + '.'
 						print 'Threshold for disk space was reached at, ' + str(alert_threshold) + '% backup was aborted.'
@@ -221,13 +211,13 @@ def is_number(s):
 	except ValueError:
 		return False
 
-def delete_old(retention, server, directory):
+def delete_old(retention, server, directory, daily_log):
 	now = datetime.datetime.now()
-	TODAY = now.strftime("%Y-%m-%d")
+	today = now.strftime("%Y-%m-%d")
 	count = 0
 	filestack = []
 	discard = []
-	bkout = open(BACKUP_LOG_DAILY, 'a', 0)
+	bkout = open(daily_log, 'a', 0)
 	for dir in os.listdir(directory):
 		try:
 			pattern = dir.split('-')
@@ -262,7 +252,7 @@ def delete_old(retention, server, directory):
 		last_element = sorted_stack[stack_length-1]
 		print "Last Element %s" %(last_element)
 		bkout.write("Last Element: %s\n" %(last_element))
-		if (TODAY == last_element):
+		if (today == last_element):
 			print "Backup of name %s already exists" %(last_element)
 			bkout.write("Backup of name %s already exists\n" %(last_element))
 			delete_num = 0
@@ -297,63 +287,63 @@ def delete_old(retention, server, directory):
 	bkout.flush()
 
 # If backup is of type "remote" then rsync over ssh.
-def backup_remote(server, directory, include_file, exclude_file, username):
-	print "Daily LOG: %s" %(BACKUP_LOG_DAILY)
-	bkout = open(BACKUP_LOG_DAILY, 'a', 0)
-	bkout.write("Daily LOG: %s\n" %(BACKUP_LOG_DAILY))
-	CURRENT = directory+"/"+server+"/daily/current"
-	DAILY_DST = directory+"/"+server+"/daily/"
-	if not os.path.exists(DAILY_DST):
-		os.makedirs(DAILY_DST)
-	INC_FILE = open(include_file, 'r')
-	for inc_line in INC_FILE:
+def backup_remote(server, directory, include_file, exclude_file, username, daily_log):
+	print "Daily LOG: %s" %(daily_log)
+	bkout = open(daily_log, 'a', 0)
+	bkout.write("Daily LOG: %s\n" %(daily_log))
+	current = directory+"/"+server+"/daily/current"
+	daily_dst = directory+"/"+server+"/daily/"
+	if not os.path.exists(daily_dst):
+		os.makedirs(daily_dst)
+	inc_file = open(include_file, 'r')
+	for inc_line in inc_file:
 		line = inc_line.rstrip()
-		print "%s --exclude-from=%s %s@%s:%s %s" %(RSYNC, exclude_file, username, server, line, CURRENT)
-		bkout.write("%s --exclude-from=%s %s@%s:%s %s\n" %(RSYNC, exclude_file, username, server, line, CURRENT))
-		process = subprocess.Popen("%s --exclude-from=%s %s@%s:%s %s\n" %(RSYNC, exclude_file, username, server, line, CURRENT), shell=True, stdout=bkout, stderr=bkout)
+		print "%s --exclude-from=%s %s@%s:%s %s" %(RSYNC, exclude_file, username, server, line, current)
+		bkout.write("%s --exclude-from=%s %s@%s:%s %s\n" %(RSYNC, exclude_file, username, server, line, current))
+		process = subprocess.Popen("%s --exclude-from=%s %s@%s:%s %s\n" %(RSYNC, exclude_file, username, server, line, current), shell=True, stdout=bkout, stderr=bkout)
 		ret_code = process.wait()
-	INC_FILE.close()
+	inc_file.close()
 	bkout.flush()
 
 # If backup is of type "local" then do a regular rsync.
-def backup_local(server, directory, include_file, exclude_file):
-	print "Daily LOG:", BACKUP_LOG_DAILY
-	bkout = open(BACKUP_LOG_DAILY, 'a', 0)
-	bkout.write("Daily LOG: %s\n" %(BACKUP_LOG_DAILY))
-	CURRENT = directory+"/"+server+"/daily/current"
-	DAILY_DST = directory+"/"+server+"/daily/"
-	if not os.path.exists(DAILY_DST):
-		os.makedirs(DAILY_DST)
-	INC_FILE = open(include_file, 'r')
-	for inc_line in INC_FILE:
+def backup_local(server, directory, include_file, exclude_file, daily_log):
+	print "Daily LOG:", daily_log
+	bkout = open(daily_log, 'a', 0)
+	bkout.write("Daily LOG: %s\n" %(daily_log))
+	current = directory+"/"+server+"/daily/current"
+	daily_dst = directory+"/"+server+"/daily/"
+	if not os.path.exists(daily_dst):
+		os.makedirs(daily_dst)
+	inc_file = open(include_file, 'r')
+	for inc_line in inc_file:
 		line = inc_line.rstrip()
-		print "%s --exclude-from=%s %s %s" %(RSYNC, exclude_file, line, CURRENT)
-		bkout.write("%s --exclude-from=%s %s %s\n" %(RSYNC, exclude_file, line, CURRENT))
-		process = subprocess.Popen("%s --exclude-from=%s %s %s\n" %(RSYNC, exclude_file, line, CURRENT), shell=True, stdout=bkout, stderr=bkout)
+		print "%s --exclude-from=%s %s %s" %(RSYNC, exclude_file, line, current)
+		bkout.write("%s --exclude-from=%s %s %s\n" %(RSYNC, exclude_file, line, current))
+		process = subprocess.Popen("%s --exclude-from=%s %s %s\n" %(RSYNC, exclude_file, line, current), shell=True, stdout=bkout, stderr=bkout)
 		ret_code = process.wait()
-	INC_FILE.close()
+	inc_file.close()
 	bkout.flush()
 
 # Hardlink files from "current" directory to todays dated directory.
 # Hardlinks here allow for incremental backups.
-def copy_to_today(server, directory, exclude_file):
+def copy_to_today(server, directory, exclude_file, daily_log):
 	now = datetime.datetime.now()
 	rightnow = now.strftime("%Y-%m-%d@%H:%M:%S")
-	TODAY = now.strftime("%Y-%m-%d")
-	bkout = open(BACKUP_LOG_DAILY, 'a', 0)
-	CURRENT = directory+"/"+server+"/daily/current"
-	NEW_DST = directory+"/"+server+"/daily/"+TODAY
-	if not os.path.exists(NEW_DST):
-		os.makedirs(NEW_DST)
-	if not os.path.exists(CURRENT):
-		os.makedirs(CURRENT)
+	today = now.strftime("%Y-%m-%d")
+	bkout = open(daily_log, 'a', 0)
+	current = directory+"/"+server+"/daily/current"
+	new_dst = directory+"/"+server+"/daily/"+today
+	if not os.path.exists(new_dst):
+		os.makedirs(new_dst)
+	if not os.path.exists(current):
+		os.makedirs(current)
 
 	# Start of adding support to remove an exclusion from "current" if it previously existed
-	EXC_FILE = open(exclude_file, 'r')
+	exc_file = open(exclude_file, 'r')
 	if os.stat(exclude_file)[6] != 1:
-		for exc_line in EXC_FILE:
+		for exc_line in exc_file:
 			line = exc_line.rstrip()
-			delete_path = CURRENT+line
+			delete_path = current+line
 			if os.path.exists(delete_path):
 				print "Delete path is: %s" %(delete_path)
 				bkout.write("Delete path is: %s\n" %(delete_path))
@@ -362,10 +352,10 @@ def copy_to_today(server, directory, exclude_file):
 				bkout.write("Exclusion path %s doesn't exist or is already removed.\n" %(delete_path))
 			rm_process = subprocess.Popen('rm -rf %s' %(delete_path), shell=True, stdout=bkout, stderr=bkout)
 			ret_code_rm = rm_process.wait()
-	EXC_FILE.close()
+	exc_file.close()
 	# End of adding support to remove an exclusion if it previously existed
 
-	process = subprocess.Popen('cp -al %s/* %s' %(CURRENT, NEW_DST), shell=True, stdout=bkout, stderr=bkout)
+	process = subprocess.Popen('cp -al %s/* %s' %(current, new_dst), shell=True, stdout=bkout, stderr=bkout)
 	ret_code = process.wait()
 	new_time = datetime.datetime.now()
 	print "*** BACKUPS FOR %s ENDED %s ***" %(server, rightnow)
@@ -388,8 +378,8 @@ def log_aggregator():
 	log_dir_stack = []
 	for section_name in parser.sections():
 		if parser.has_option(section_name, 'log_dir'):
-			LOG_DIR = parser.get(section_name, 'log_dir')
-			log_dir_stack.append(LOG_DIR)
+			log_dir = parser.get(section_name, 'log_dir')
+			log_dir_stack.append(log_dir)
 	log_dir_stack = uniq(log_dir_stack) # Re-Initialize log_dir_stack with unique entries.
 	for log_dir in log_dir_stack:
 		filestack = []
@@ -404,5 +394,8 @@ def log_aggregator():
 				backup_daily_master_log.write(line)
 		backup_daily_master_log.close()
 
-parse_config()
-log_aggregator()
+
+if __name__ == "__main__":
+#	main(sys.argv[1:])
+	parse_config()
+	log_aggregator()
